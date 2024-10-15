6eabbb37ad186a04af4330f0f8ee1afb1f3fbf23
chineduede <chndvz@gmail.com>
2024-10-15 09:11:51 +0100

    handle multiple pongs

--- app/main.py
+++ app/main.py
@@ -4,14 +4,18 @@ import socket  # noqa: F401
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
+    response = b'+PONG\r\n'
+    connections: list[socket.socket] = []
 
     # Uncomment this to pass the first stage
     #
     with socket.create_server(("localhost", 6379), reuse_port=True) as server:
         # while True:
         conn, _ = server.accept() # wait for client
-        conn.sendall(b'+PONG\r\n')
+        connections.append(conn)
 
+        while conn.recv(1024):
+            conn.sendall(response)
 
 if __name__ == "__main__":
     main()
